781efc7ebb644ffb6ab0c4fb46513ba08831bed6
imrankhan919 <imrankhan919.ik@gmail.com>
2026-02-19 15:54:19 +0530

    authentication add and error handler middleware introduced

---
+++ server/controller/authController.js
@@ -0,0 +1,72 @@
+import bcrypt from "bcryptjs"
+import User from "../models/userModel.js"
+
+const registerUser = async (req, res) => {
+
+    const { name, email, phone, password } = req.body
+
+    // Check if all fileds are coming
+    if (!name || !email || !phone || !password) {
+        res.status(409)
+        throw new Error("Please Fill All Details!")
+    }
+
+    // Check if user exist
+    let emailExist = await User.findOne({ email: email })
+    let phoneExist = await User.findOne({ phone: phone })
+
+
+    if (emailExist || phoneExist) {
+        res.status(400)
+        throw new Error("User Already Exists!")
+    }
+
+
+    // Hash Password 
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    const user = await User.create({
+        name,
+        email,
+        phone,
+        password: hashedPassword
+    })
+
+    if (!user) {
+        res.status(400)
+        throw new Error("User not created!")
+    }
+
+    res.status(201).json(user)
+
+
+}
+
+
+const loginUser = async (req, res) => {
+    const { email, password } = req.body
+
+    // Check if all fileds are coming
+    if (!email || !password) {
+        res.status(409)
+        throw new Error("Please Fill All Details!")
+    }
+
+    // Check if user exist
+    let user = await User.findOne({ email: email })
+
+
+    if (user && await bcrypt.compare(password, user.password)) {
+        res.status(200).json(user)
+    } else {
+        res.status(401)
+        throw new Error("Invalid Credentials!")
+    }
+
+}
+
+
+const authController = { registerUser, loginUser }
+
+export default authController
--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 import express from "express"
 import dotenv from "dotenv"
 import colors from "colors"
+
+// Local Imports
+import authRoutes from "./routes/authRoutes.js"
 import connectDB from "./config/dbConfg.js"
+import errorHandler from "./middleware/errorHandler.js"
+
+
 
 dotenv.config()
 
@@ -9,9 +15,15 @@ dotenv.config()
 connectDB()
 
 
-const PORT = process.env.PORT || 5000
 
+
+const PORT = process.env.PORT || 5000
 const app = express()
+
+
+// Body-Parser
+app.use(express.json())
+app.use(express.urlencoded())
 
 
 // Default Route
@@ -21,5 +33,13 @@ app.get("/", (req, res) => {
     })
 })
 
+
+
+// Auth Routes
+app.use("/api/auth", authRoutes)
+
+
+// Error Handler
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`SERVER IS RUNNING AT PORT : ${PORT}`.bgBlue.white))
